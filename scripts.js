4582115bb759de5ce285bb34985c01f6d48e06b4
venkat662433 <97409925+venkat662433@users.noreply.github.com>
2025-01-11 16:34:22 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -48,4 +48,19 @@ document.addEventListener('DOMContentLoaded', () => {
             hobby.classList.toggle('flipped');
         });
     });
+
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
 });
