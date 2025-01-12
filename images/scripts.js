72d9f5b70eeeccd2a246a4ed9287412fdd46c96e
venkat662433 <97409925+venkat662433@users.noreply.github.com>
2025-01-12 18:13:55 +0100

    Add files via upload

---
+++ images/scripts.js
@@ -0,0 +1,66 @@
+// Select the label and contact form
+const contactLabel = document.querySelector('contact, .contact-us-btn',);
+const contactForm = document.getElementById('contact-form');
+
+// Add click event listener to the label
+contactLabel.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default behavior (if any)
+    
+    // Scroll to the contact form
+    contactForm.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
+
+function scrollToSection(homepage) {
+    document.getElementById(homepage).scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSection(edu1) {
+    document.getElementById(edu1).scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSection(aboutme) {
+    document.getElementById(aboutme).scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSection(proj) {
+    document.getElementById(proj).scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSection(exp) {
+    document.getElementById(exp).scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSection(edu1) {
+    document.getElementById(edu1).scrollIntoView({ behavior: 'smooth' });
+}
+
+//-------------------- hobbies Animation-------------------------------
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hobbies = document.querySelectorAll('.hobby-item');
+
+    hobbies.forEach(hobby => {
+        hobby.addEventListener('click', () => {
+            hobby.classList.toggle('flipped');
+        });
+    });
+
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+});
